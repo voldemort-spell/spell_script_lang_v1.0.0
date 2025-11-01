cfa29023cf06b7b445b03a73ef409dca92a1ef25
thushan-123 <thushanmadusanka456@gmail.com>
2025-11-01 20:11:55 +0530

    createnew Lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -4,14 +4,15 @@ package lexer
 
 type Lexer struct {
 	input       string
-	position    int 		// current position input
-	readPostion int			//curr reading position
-	ch          rune		//curr char under examin
+	position    int  // current position input
+	readPostion int  //curr reading position
+	ch          rune //curr char under examin
 	pos         Position
 }
 
 // create new lexer
 
-
-
-
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	return l
+}
