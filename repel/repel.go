f97691c0c834e60cff67c6b297743995cd49b6fb
thushan-123 <thushanmadusanka456@gmail.com>
2025-11-01 22:30:10 +0530

    crete REPL

--- repel/repel.go
+++ repel/repel.go
@@ -2,11 +2,36 @@ package repel
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"spell_script_lang/lexer"
 )
+
+const BANNER = "Welcome to Magic World."
 
 const PROMPT = ">>> "
 
+// start funtion REPL
 func Start() {
 	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print(PROMPT)
+		scanned := scanner.Scan()
+
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.NewLexer(line)
+
+		for {
+			token := l.NextToken()
+			if token.Type == lexer.EOF {
+				break
+			}
+			fmt.Printf("%s: %s \n", token.Type, token.Literal)
+		}
+	}
 }
