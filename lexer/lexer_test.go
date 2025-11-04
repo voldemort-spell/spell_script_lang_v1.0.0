02b250e08c674708acc7aecd1a9a7fa19832369a
thushan-123 <thushanmadusanka456@gmail.com>
2025-11-04 22:46:17 +0530

    update readCharacter

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,24 +1,36 @@
 package lexer
 
-// import (
-// 	"go/token"
-// 	"testing"
-// )
+import (
+	"testing"
+)
 
-// func TestNextToken(t *testing.T){
-// 	input := `=+(){};`
+func TestNextToken(t *testing.T) {
+	input := `==+()`
 
-// 	tests := []struct {
-// 		expectType TokenType
-// 		expectLiteral string 
-// 	}{
-// 		{ASSIGN, "="},
-// 		{PLUS, "+"},
-// 		{LPAREN, "("},
-// 		{RPAREN, ")"},
-// 		{RBRACE, "{"},
-// 		{LBRACE, "}"},
-// 		{SEMICO, ";"},
-// 	}
+	test := []struct {
+		expectedType    TokenType
+		expectedLiteral string
+	}{
+		{EQUAL, "=="},
+		{PLUS, "+"},
+		{LPAREN, "("},
+		{RPAREN, ")"},
+	}
 
-// }
+	lex := NewLexer(input)
+
+	for i, tt := range test {
+		tk := lex.NextToken()
+
+		if tk.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - token type wrong. expected=%q, got=%q",
+				i, tt.expectedType, tk.Type)
+		}
+
+		if tk.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tk.Literal)
+		}
+
+	}
+}
