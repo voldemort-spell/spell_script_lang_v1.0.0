795b0842c5852c7fb37631f2243c9026b26b3927
thushan-123 <thushanmadusanka456@gmail.com>
2025-11-05 08:39:28 +0530

    create new test case

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -33,4 +33,51 @@ func TestNextToken(t *testing.T) {
 		}
 
 	}
+
+	input1 = `
+		v  value1 = 100
+		v  value2 = -120
+
+		v sumeation = fn(x, y){
+			return x + y
+		}
+
+		print(sumation)
+	`
+	lex1 := NewLexer(input1)
+
+	tk1 := lex1.NextToken()
+	if tk1.Type != VAR {
+		t.Fatalf("expected VAR, got %q", tk1.Type)
+	}
+	if tk1.Literal != "v" {		t.Fatalf("expected 'v', got %q", tk1.Literal)
+	}
+
+	tk2 := lex1.NextToken()
+	if tk2.Type != IDENT {
+		t.Fatalf("expected IDENT, got %q", tk2.Type)
+	}
+	if tk2.Literal != "value1" {
+		t.Fatalf("expected 'value1', got %q", tk2.Literal)
+	}
+	tk3 := lex1.NextToken()
+	if tk3.Type != ASSIGN {
+		t.Fatalf("expected ASSIGN, got %q", tk3.Type)
+	}
+	if tk3.Literal != "=" {
+		t.Fatalf("expected '=', got %q", tk3.Literal)
+	}
+
+	tk4 := lex1.NextToken()
+	if tk4.Type != INT {
+		t.Fatalf("expected INT, got %q", tk4.Type)
+	}
+	if tk4.Literal != "100" {
+		t.Fatalf("expected '100', got %q", tk4.Literal)
+	}
+	tk5 := lex1.NextToken()
+	if tk5.Type != SEMICOLON {
+		t.Fatalf("expected SEMICOLON, got %q", tk5.Type)
+	// v value1 = 100
+
 }
