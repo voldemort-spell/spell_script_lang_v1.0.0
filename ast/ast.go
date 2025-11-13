302fdd6e78bac1c278d2edd2e66de0bbd748f79e
thushan-123 <thushanmadusanka456@gmail.com>
2025-11-13 22:45:29 +0530

    define statement , expression

--- ast/ast.go
+++ ast/ast.go
@@ -1,7 +1,19 @@
 package ast
 
+// AST - abstract tree syntax
+// v <identifier> = <expression>;
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
+type Statement interface {
+	Node
+	StatementNode()
+}
+
+type Expression interface {
+	Node
+	ExpressionNode()
+}
