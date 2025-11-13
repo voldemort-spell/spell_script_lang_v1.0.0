2df6ca6a240ff4680432f65e09e8b8c640109892
thushan-123 <thushanmadusanka456@gmail.com>
2025-11-13 23:07:31 +0530

    ident , var

--- ast/ast.go
+++ ast/ast.go
@@ -1,5 +1,7 @@
 package ast
 
+import "go/token"
+
 // AST - abstract tree syntax
 // v <identifier> = <expression>;
 
@@ -16,4 +18,26 @@ type Statement interface {
 type Expression interface {
 	Node
 	ExpressionNode()
+}
+
+type Programe struct {
+	Statement []Statement
+}
+
+func (program *Programe) TokenLiteral() string {
+	if len(program.Statement) > 0 {
+		return program.Statement[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type Ident struct {
+	Token token.Token
+	Value string
+}
+
+type VarStatement struct {
+	Token token.Token
+	Name  *Ident
 }
